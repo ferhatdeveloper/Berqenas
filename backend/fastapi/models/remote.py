589de83223314554017f28128f2fd89776ed81ea
ferhatdeveloper <ferhatdeveloper@gmail.com>
2026-01-16 13:01:07 +0300

    Fix NameError func in remote.py

--- backend/fastapi/models/remote.py
+++ backend/fastapi/models/remote.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Text
+from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
